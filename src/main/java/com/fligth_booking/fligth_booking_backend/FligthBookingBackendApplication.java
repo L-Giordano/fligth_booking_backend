3be9b94d8f75cdb836f547d5a7c9f7ac0f08b505
Lucas Giordano <lucas@accessin.net>
2023-04-18 11:09:14 -0300

    spring boot project init

---
+++ src/main/java/com/fligth_booking/fligth_booking_backend/FligthBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fligth_booking.fligth_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FligthBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FligthBookingBackendApplication.class, args);
+	}
+
+}
